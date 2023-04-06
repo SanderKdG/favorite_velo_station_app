690b31294cb66203f48693d27884faf6a15a0dfd
Sander Constantin <sander.constantin@student.kdg.be>
2023-04-06 08:49:46 +0200

    principal

--- VeloLiker/src/main/java/com/example/VeloLiker/controllers/HomeController.java
+++ VeloLiker/src/main/java/com/example/VeloLiker/controllers/HomeController.java
@@ -3,10 +3,12 @@ package com.example.VeloLiker.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HomeController {
     @GetMapping("/home/get")
-    public String home() {
-        return "Hello, JWT";
+    public String home(Principal principal) {
+        return "Hello, "+principal.getName();
     }
 }
