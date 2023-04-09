3fd4d94d2be4b8c16e93d4734128bc59758d27c9
Sander Constantin <sander.constantin@student.kdg.be>
2023-04-09 12:17:21 +0200

    integrated test

---
+++ VeloLiker/src/test/java/com/example/VeloLiker/util/integrated/StationRestClientTests.java
@@ -0,0 +1,28 @@
+package com.example.VeloLiker.util.integrated;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+import com.example.VeloLiker.util.StationRestClient;
+import java.util.Objects;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StationRestClientTests {
+  @Autowired
+  private StationRestClient stationRestClient;
+
+  @Test
+  void getStations() {
+    var response = stationRestClient.getStations();
+    assertEquals("Has thrown error", false ,response.getStatusCode().isError());
+    assertEquals("Status code", 200 ,response.getStatusCode().value());
+    assertNotNull("Body is not null", response.getBody());
+    assertNotNull("Network is not null", Objects.requireNonNull(response.getBody()).getNetwork());
+    assertNotNull("Stations is not null", response.getBody().getNetwork().getStations());
+    assertTrue("At least 1 station", response.getBody().getNetwork().getStations().size() > 0);
+  }
+}
