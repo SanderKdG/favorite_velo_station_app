cec29564b66c20a2bf18c8bf91f9f2531a524980
Sander Constantin <58855319+SanderCC@users.noreply.github.com>
2024-06-26 14:25:11 +0200

    Update setupHttp.ts

--- reactjs/src/functions/setupHttp.ts
+++ reactjs/src/functions/setupHttp.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import {getAuthToken} from "./authStorage";
 
 export default function setupHttp() {
-    axios.defaults.baseURL = "http://localhost:8080"
+    axios.defaults.baseURL = "https://veloliker-java.sanderc.net"
     if(getAuthToken() !== null) axios.defaults.headers['Authorization'] = `Bearer ${getAuthToken()}`
     else axios.defaults.headers['Authorization'] = null
 }
